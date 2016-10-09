bb5458679f12f57310c70b4dc9ccc7be1fc51d61
soumen-saniel <soumen.saniel@gmail.com>
2016-10-10 00:22:03 +0530

    added iscroll for mobile

--- app-scripts/app.js
+++ app-scripts/app.js
@@ -1,17 +1,43 @@
-$(document).foundation();
-$(window).stellar();
-$(document).ready(function(){
-	var windowHeight = $(window).height();
-	$(".fullHeight").css({"height" : windowHeight});
-	$(".halfHeight").css({"height" : (windowHeight/2)});
+(function(){
+	var ua = navigator.userAgent,
+	isMobileWebkit = /WebKit/.test(ua) && /Mobile/.test(ua);
 
-	$(".toggle-menu").on("click", function(){
-		$("#menu").toggleClass("is-open");
-		$(".wrapper").toggleClass("blure");
+	if (isMobileWebkit) {
+		$('html').addClass('mobile');
+	}
+
+	$(function(){
+		var iScrollInstance;
+
+		if (isMobileWebkit) {
+			iScrollInstance = new iScroll('wrapper');
+
+			$('#scroller').stellar({
+				scrollProperty: 'transform',
+				positionProperty: 'transform',
+				horizontalScrolling: false,
+				verticalOffset: 150
+			});
+		} else {
+			$.stellar({
+				horizontalScrolling: false,
+				verticalOffset: 150
+			});
+		}
+	});
+	$(document).ready(function(){
+		var windowHeight = $(window).height();
+		$(".fullHeight").css({"height" : windowHeight});
+		$(".halfHeight").css({"height" : (windowHeight/2)});
+
+		$(".toggle-menu").on("click", function(){
+			$("#menu").toggleClass("is-open");
+			$(".wrapper").toggleClass("blure");
+		});
+
+		setTimeout(function(){
+			$(".landing-text").removeClass("slide-in-text");
+		}, 1000);
 	});
 
-	setTimeout(function(){
-		$(".landing-text").removeClass("slide-in-text");
-	}, 1000);
-
-});
+})();
