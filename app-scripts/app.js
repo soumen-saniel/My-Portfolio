9e90e813c63b89eb3ca5e1ca30a34d1cafff4aca
soumen-saniel <soumen.saniel@gmail.com>
2016-10-10 00:51:32 +0530

    app.js changed

--- app-scripts/app.js
+++ app-scripts/app.js
@@ -1,30 +1,24 @@
 (function(){
-	var ua = navigator.userAgent,
-	isMobileWebkit = /WebKit/.test(ua) && /Mobile/.test(ua);
+	// var ua = navigator.userAgent,
+	// isMobileWebkit = /WebKit/.test(ua) && /Mobile/.test(ua);
 
-	if (isMobileWebkit) {
-		$('html').addClass('mobile');
-	}
+	// $(function(){
+	// 	var iScrollInstance;
 
-	$(function(){
-		var iScrollInstance;
+	// 	if (isMobileWebkit) {
+	// 		iScrollInstance = new iScroll('scroller');
 
-		if (isMobileWebkit) {
-			iScrollInstance = new iScroll('wrapper');
-
-			$('#scroller').stellar({
-				scrollProperty: 'transform',
-				positionProperty: 'transform',
+	// 		$('#scroller').stellar({
+	// 			scrollProperty: 'transform',
+	// 			positionProperty: 'transform',
+	// 			horizontalScrolling: false
+	// 		});
+	// 	} else {
+			$(window).stellar({
 				horizontalScrolling: false,
-				verticalOffset: 150
 			});
-		} else {
-			$.stellar({
-				horizontalScrolling: false,
-				verticalOffset: 150
-			});
-		}
-	});
+	// 	}
+	// });
 	$(document).ready(function(){
 		var windowHeight = $(window).height();
 		$(".fullHeight").css({"height" : windowHeight});
