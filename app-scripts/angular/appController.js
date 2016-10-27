51a16e4b448a92ab4bf8155618eb33721240fa6f
soumen-saniel <soumen.saniel@gmail.com>
2016-10-28 01:50:50 +0530

    commit

---
+++ app-scripts/angular/appController.js
@@ -0,0 +1,11 @@
+(function(){
+
+var app = angular.module("appModule", []);
+
+app.controller("appController", ["$scope", appController]);
+
+function appController($scope){
+	
+}
+
+})();
---
+++ app-scripts/angular/appService.js
@@ -0,0 +1,11 @@
+(function(){
+
+var app = angular.module("appModule");
+
+app.factory('name', ['', function(){
+	return function name(){
+		
+	};
+}])
+
+})();
